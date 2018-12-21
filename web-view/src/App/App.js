abba98e2de20c4980d2491a67fde4dd4316422ea
守屋千春 <mry116k@hotmail.com>
2018-12-21 23:20:35 +0900

    axiosの動作確認

--- web-view/src/App/App.js
+++ web-view/src/App/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+
+const API_GET = 'https://api.github.com';
 
 class App extends Component {
   render() {
+
+    axios
+    .get(API_GET, {})
+    .then((results) => {
+      console.log(results);
+      }
+    )
+    .catch(() => {
+      console.log('通信に失敗しました');
+    });
+
     return (
       <div className="App">
         <header className="App-header">
